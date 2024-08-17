7d532c3691f2b9aa813d0d9f1000432a344a1b60
Brian Wakhale <Brianweke1998@gmail.com>
2024-08-17 16:53:19 +0300

    Completed task 1

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer style={{
+  
+
+      background: '#333',
+      color: '#fff',
+      padding: '5px',
+      textAlign: 'center',
+      position: 'fixed',
+      bottom: 0,
+      width: '100%',
+    
+}}>
+  <p>© 2023 City Lovers</p>
+</footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <main style={{
+      backgroundColor: '#f1f1f1',
+      padding: '10px',
+      borderRadius: '10px',
+      margin: '10px',
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',  // Drop shadow effect with a slight blur and transparency
+      color: '#333',  // Color Grey
+    }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
