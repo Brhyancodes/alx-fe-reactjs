10ddbf2c807af32ed801096d4531bcf36bfb2aef
Brian Wakhale <Brianweke1998@gmail.com>
2024-08-17 16:39:40 +0300

    Completed the Final Task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Footer />
       
       <WelcomeMessage />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       
       
       <div>
