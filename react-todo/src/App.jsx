dfa614e6a93421e798db3c87f3f6e653da8def58
Brian Wakhale <brianweke1998@gmail.com>
2024-08-31 22:14:02 +0300

    Completed react-to-do task

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Lorem ipsum", completed: false },
+    { id: 2, title: "dolor sit amet", completed: false },
+  ]);
+  return (
+    <>
+      <TodoList todos={todos} setTodos={setTodos} />
+      <AddTodoForm todos={todos} setTodos={setTodos} />
+    </>
+  );
+}
+
+export default App;
